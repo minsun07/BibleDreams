524ab24305e3dccc32d256e16fcb9adb74ffcacf
Minsun An <elleto123456@gmail.com>
2025-08-29 09:58:54 +0900

    #4 기록 화면 디자인록 화면 디자인 - 내역 불러오는 버튼 - 기록 화면

--- FormHistory.Designer.cs
+++ FormHistory.Designer.cs
@@ -28,24 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.lbHistory = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // btnSend
             // 
-            this.groupBox1.Location = new System.Drawing.Point(16, 45);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(360, 391);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.btnSend.Location = new System.Drawing.Point(25, 18);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(341, 45);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "내역 불러오기";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // lbHistory
+            // 
+            this.lbHistory.FormattingEnabled = true;
+            this.lbHistory.ItemHeight = 12;
+            this.lbHistory.Items.AddRange(new object[] {
+            "2025-08-24 13:00:01 - 20071201 03 - 길을 잃고 헤매다가 빛을 따라 걷는 꿈",
+            "2025-08-24 13:01:01 - 20071201 03 - 낡은 집을 깨끗하게 청소하고 새롭게 꾸미는 꿈",
+            "2025-08-24 13:03:01 - 20071201 03 - 사막 한가운데서 오아시스를 발견하는 꿈",
+            "2025-08-24 13:05:01 - 20071201 03 - 물이 가득 찬 맑고 투명한 바다를 걷는 꿈",
+            "2025-08-24 13:06:01 - 20071201 03 - 높은 절벽 끝에 서 있는데, 아래로 떨어지는 꿈"});
+            this.lbHistory.Location = new System.Drawing.Point(24, 83);
+            this.lbHistory.Name = "lbHistory";
+            this.lbHistory.Size = new System.Drawing.Size(341, 328);
+            this.lbHistory.TabIndex = 1;
             // 
             // FormHistory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(393, 450);
+            this.Controls.Add(this.lbHistory);
+            this.Controls.Add(this.btnSend);
             this.Name = "FormHistory";
             this.Text = "FormHistory";
             this.ResumeLayout(false);
@@ -54,6 +71,7 @@
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ListBox lbHistory;
     }
 }
